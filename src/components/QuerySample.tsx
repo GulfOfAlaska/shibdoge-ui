c7dfadb89ad2703cda60cbf3533152172f3cbc6f
PC <spclight@gmail.com>
2021-10-31 01:03:52 +0800

    Query side properly

--- src/components/QuerySample.tsx
+++ src/components/QuerySample.tsx
@@ -10,7 +10,10 @@ import { Withdraw } from './Withdraw';
 import './componentStyle.css'
 
 interface SideResponse {
-  side: number
+  side: {
+    current_winning_count: number,
+    total_amount: number
+  }
 }
 
 export function QuerySample() {
@@ -31,7 +34,7 @@ export function QuerySample() {
     });
   }, [connectedWallet]);
 
-  async function getChosenSide(address: string): Promise<SideResponse | undefined> {
+  async function getChosenSide(address: string): Promise<any | undefined> {
     return lcd?.wasm.contractQuery(
       contractAddress,
       { stake: { address } }
@@ -47,22 +50,31 @@ export function QuerySample() {
 
   useInterval(
     async () => {
-      if (!connectedWallet?.terraAddress) return
-      const side: SideResponse | undefined = (await getChosenSide(connectedWallet?.terraAddress.toString()))
-      if (!side?.side) {
-        return
-      }
-      if (side.side === 1) {
-        setChosenSide('doge')
-      }
-      if (side?.side === 2) {
-        setChosenSide('shiba')
+      // try {
+      //   if (!connectedWallet?.terraAddress) return
+      //   const side= (await getChosenSide(connectedWallet?.terraAddress.toString()))
+      //   if (!side?.side) {
+      //     return
+      //   }
+      //   if (side.side === 1) {
+      //     setChosenSide('doge')
+      //   }
+      //   if (side?.side === 2) {
+      //     setChosenSide('shiba')
+      //   }
+      // } catch (err) {
+      //   console.error(err)
+      // }
+
+      try {
+        const dogeScore: SideResponse | undefined = await getScore(1)
+        setDogeScore(`Winning count: ${dogeScore?.side?.current_winning_count} Current count: ${dogeScore?.side?.total_amount}`)
+        const shibaScore: SideResponse | undefined = await getScore(2)
+        setShibaScore(`Winning count: ${shibaScore?.side?.current_winning_count} Current count: ${shibaScore?.side?.total_amount}`)
+      } catch (err) {
+        console.error(err)
       }
 
-      const dogeScore: any = (await getScore(1))
-      setDogeScore(JSON.stringify(dogeScore))
-      const shibaScore: any = (await getScore(2))
-      setShibaScore(JSON.stringify(shibaScore))
     },
     3000,
   )
